3bc1754dafb9b742946bcb15adab2f1c686d4cfa
w4-uhq <zayixx02@gmail.com>
2025-09-03 17:28:59 +0000

    add

---
+++ tpsallestaff.lua
@@ -0,0 +1,7 @@
+layer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+    local hrp = player.Character.HumanoidRootPart
+    hrp.CFrame = CFrame.new(-0.20784521102905273, 595.2616577148438, 55.65902328491211)
+else
+end
+
+    print("cbn mon reuf")
